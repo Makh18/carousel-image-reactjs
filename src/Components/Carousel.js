e6cac03168e301170c5759f6e30270bf941efe68
Mabrouka <khalfamarouka@gmailcom>
2023-08-23 11:14:12 +0100

    first commit

---
+++ src/Components/Carousel.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "../carousel.css";
+import { imgSlider } from "../constants/index";
+import { FiArrowLeftCircle } from "react-icons/fi";
+import { FiArrowRightCircle } from "react-icons/fi";
+
+const Carousel = () => {
+  const [currentImg, setCurrentImg] = useState(0);
+
+  return (
+    <div className="carousel">
+      <div
+        className="carouselInner"
+        style={{ backgroundImage: `url(${imgSlider[currentImg].imgUrl})` }}
+      >
+        <div className="overlay">
+          <div
+            className="left"
+            onClick={() => {
+              currentImg > 0 && setCurrentImg(currentImg - 1);
+            }}
+          >
+            <FiArrowLeftCircle style={{ fontSize: 30 }} />
+          </div>
+          <div
+            className="right"
+            onClick={() => {
+              currentImg < imgSlider.length - 1 &&
+                setCurrentImg(currentImg + 1);
+            }}
+          >
+            <FiArrowRightCircle style={{ fontSize: 30 }} />
+          </div>
+        </div>
+      </div>
+      Carousel
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+export const imgSlider = [
+    {
+      id: "01",
+      imgUrl: "/7.jpg"
+    },
+    {
+      id: "02",
+      imgUrl: "/8.jpg"
+    },
+    {
+      id: "03",
+      imgUrl: "/6.jpg"
+    }
+  ];
+  
